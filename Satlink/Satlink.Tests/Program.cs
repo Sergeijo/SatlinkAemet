4b68e1248e4a8420688439ce5e49be2938eeda25
Sergio Álvarez <sergeijo@outlook.com>
2025-12-19 22:39:02 +0100

    fix: launch mode changed

--- Satlink/Satlink.Tests/Program.cs
+++ Satlink/Satlink.Tests/Program.cs
@@ -4,7 +4,8 @@ namespace Satlink.Tests
 {
     internal class Program
     {
-        public static void Main()
+        // Renamed to avoid CS0017 (multiple entry points when combined with WPF-generated entry point)
+        public static void Run()
         {
             TestRunner.RunAll();
             Console.WriteLine("Press any key to exit...");
